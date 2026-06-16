cec402578eec57c160deda9ee037e326773f69dc
Drin Musa <drinmusa99@hotmail.com>
2026-06-16 12:04:02 +0200

    Remove v1 from routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,7 +5,7 @@ import { authRouter } from './routes/auth';
 import { uploadRouter } from './routes/upload';
 
 export const routes = (app: Application): void => {
-    app.use('/api/v1/ping', PingController);
+    app.use('/api/ping', PingController);
     app.use('/api/auth', authRouter);
     app.use('/api/uploads', uploadRouter);
 };
